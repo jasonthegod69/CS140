e2dad171039aea8ea4d2ad4ca739d447942033ef
Boyuan Chen <boyuanchen@Boyuans-MacBook-Air.local>
2020-03-10 00:23:28 -0700

    added comments to main

--- HW11_buildhigh/main.py
+++ HW11_buildhigh/main.py
@@ -1,13 +1,18 @@
-
+# Boyuan(Jack) Chen  &  Jason Phillips
+# 2020/03/10 Tuesday
+# The code comprises of two parts: the main file, where you run the code; and the
+# toolkit file, where the functions are stored. We have explanary comments in both
+# files.
 from toolkit import *
 
+# The input is by default input.txt. Please enter the values there.
 fName = "input.txt"
-#fName = input("Enter input file name: ")
 f = open(fName, 'r')
 numBlocks = int(f.readline())
 blockArray = []
 maxL = 0
-# Read in the input file
+
+# ------ Read in the input file ------
 for line in f:
   if numBlocks==0:
     break
@@ -20,24 +25,36 @@ for line in f:
   blockArray.append(aBlock)
   numBlocks-=1
 
+# ------ Get the sorted, clean, combination list ------
+# Find all combinations of edge length all the objects. Put them in a single array
 all_combs    = get_all_combinations(blockArray)
 print("All combinations are: ")
 print(all_combs)
 print()
+# Delete ones that are repetitive and each comb that [0]<[1].
+# Say we have [3,5,7], then we would delete [5,3,7], because it is symmetric
 clean_combs  = clean_repetitive_and_reverse(all_combs)
 print("After deleting the reverse and repetitives: ")
 print(clean_combs)
 print()
+# Sort in ascending order of [0]. If same, sort in [1].
+# Note that you have to build a tower in the sequence of this array.
+# There is no repetition or missing possibilities.
 sorted_combs = sort_in_x(clean_combs)
 print("After sorting in terms of the first entry: ")
 print(sorted_combs)
 print()
 
-print("\n Running... \n\n")
-
+# ------ Build Dynamic Table ------
+# Build dynamic table of each of the sorted_combs. The value means "The greatest
+# height you can get up to this block". For each new block, check the previous blocks
+# that are "clearly smaller"(see toolkit) than the current one, and find the maximum;
+# then, add the previous height with its own height.
 DP = build_dynamic_table(sorted_combs)
+print("\n Running... \n\n")
 print("Dynamic table: ")
 print(DP)
+
 
 max_height = max_DP(DP)
 trace_back = traceBack(max_height, DP)
@@ -46,9 +63,10 @@ print("Trace back: ")
 print(trace_back)
 print()
 print()
+
+# ------ Print and output the answers ------
 print("-------- Answers -------")
 print(f"\nThe tallest tower has {len(trace_back)} blocks and a height of {max_height[0]}")
-
 blocks = []
 for i in trace_back:
   blocks.append(sorted_combs[i])
@@ -62,6 +80,6 @@ for block in blocks:
   o.write("\n")
 print("You can also see it in the output.txt file")
 
-# Write to output
+# Close the files
 o.close()
 f.close()
